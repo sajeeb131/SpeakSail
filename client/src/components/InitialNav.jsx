2570304c5f0c87e73370df19a5a681cd53a205ee
kh-abrar <kh.abrarr@gmail.com>
2024-02-04 22:14:51 +0600

    Initial Navbar in landing page and other assets

---
+++ client/src/components/InitialNav.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../assets/images/LogoWhite.png'
+import './navbar.css'
+
+const InitialNav = () => {
+    const navigate = useNavigate();
+    const onClick = () =>{
+        navigate('/')
+    }
+  return (
+    <>
+        <div className='navbar'>
+            <div className='logo'>
+                <img src={Logo} alt='Logo'/>
+            </div>
+            <ul className='nav-link'>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to="/objective">Objective</Link></li>
+                <li><Link to="/what-we-offer">What We Offer</Link></li>
+                <li><Link to="/tools">Tools</Link></li>
+                <li><Link to="/about-us">About us</Link></li>
+            </ul>
+            <div className='buttons'>
+                <button className='btn login-btn'>Login</button>
+                <button className='btn signup-btn'>Signup</button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default InitialNav
---
+++ client/src/pages/landing-page/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link, useNavigate } from 'react-router-dom'
+import InitialNav from '../../components/InitialNav'
+
+
+const LandingPage = () => {
+  return (
+    <div>
+        <InitialNav/>
+    </div>
+  )
+}
+
+export default LandingPage
