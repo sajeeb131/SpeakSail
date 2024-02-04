e5adae5b0f5c592c6b5c4432a91c73b73e2639bc
kh-abrar <kh.abrarr@gmail.com>
2024-02-04 22:34:59 +0600

    footer added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from '../assets/images/logo.png'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+    <div className="footer-content">
+      <div className="logo-container">
+        <img src={Logo} alt="SpeakSail Logo" className="logo" />
+        <p className="tagline">Setting Sail to Language Mastery</p>
+      </div>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
--- client/src/pages/landing-page/LandingPage.jsx
+++ client/src/pages/landing-page/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Link, useNavigate } from 'react-router-dom'
 import InitialNav from '../../components/InitialNav'
-
+import Footer from '../../components/Footer'
 
 const LandingPage = () => {
   return (
     <div>
         <InitialNav/>
+        <h1>LandingPage</h1>
+        <Footer/>
     </div>
   )
 }
