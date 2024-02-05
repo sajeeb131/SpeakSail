f4cc3d678b90ed4b0626d0e50775b3f2e1c24e2b
sajeeb131 <sajeeb131sarkar@gmail.com>
2024-02-05 21:04:49 +0600

    Landing page 2nd portion(objectives)

--- client/src/pages/landing-page/LandingPage.jsx
+++ client/src/pages/landing-page/LandingPage.jsx
@@ -4,10 +4,15 @@ import InitialNav from '../../components/InitialNav';
 import Footer from '../../components/Footer';
 import image from '../../assets/images/header image.png'; 
 import "./LandingPage.css"
+import icon1 from "../../assets/icons/objective1.png"
+import icon2 from "../../assets/icons/objective2.png"
+import icon3 from "../../assets/icons/objective3.png"
+import icon4 from "../../assets/icons/objective4.png"
 const LandingPage = () => {
   return (
     <div>
       <InitialNav />
+      {/* get started part */}
       <div className='container-getStarted'>
         <div className='container-getStarted-half'>
           <div className='container-getStarted-half-heading'>
@@ -23,6 +28,44 @@ const LandingPage = () => {
           <img src={image} alt='' width={600} height={400}/>
         </div>
       </div>
+      {/* objective part */}
+      <div className='container-objective'>
+        <div className='container-objective-heading'><h1>Our <span style={{ color: '#FABC2A' }}>Objective</span></h1></div>
+        <div className='container-objective-points'>
+          <div className='container-objective-points-indv'>
+            <img src={icon1} alt='' width={40} height={40}/>
+            <div className=''>
+              <p>To improve the oral English communication of Bangladeshi students</p>
+            </div>
+          </div>
+          
+          <div className='container-objective-points-indv'>
+            <img src={icon2} alt='' width={40} height={40}/>
+            <div className=''>
+              <p>To enhance English language 
+                learning, focusing on 
+              listening and speaking skills</p>
+            </div>
+          </div>
+          <div className='container-objective-points-indv'>
+            
+            <img src={icon3} alt='' width={40} height={40}/>
+            <div className=''>
+              <p>To assist students to prepare for real-life situations where English communication is inevitable in this era of globalisation.</p>
+            </div>
+          </div>
+          <div className='container-objective-points-indv'>
+            
+            <img src={icon4} alt='' width={35} height={40}/>
+            
+            <div className=''>
+              <p>To cater the needs of students
+                who require further input about learning beyond classroom.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
       <Footer />
     </div>
   );
