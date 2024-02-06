9e46dc539fbf55ac59758af6fd41ac78ccdc9c8c
sajeeb131 <sajeeb131sarkar@gmail.com>
2024-02-06 22:46:21 +0600

    homepage design level1(upto progress bar)

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
-
+import './HomePage.css'
 const HomePage = () => {
+    const [username, setUsername] = useState("Abrar");
+    const [overallProgress, setOverallProgress] = useState(50);
+    const progressStyle = {
+        width: `${overallProgress}%`
+    };
   return (
-    <div>
+    <div className='container-homepage'>
         <Navbar/>
+        {/* part 1: heading */}
+        <div className='container-homepage-header'>
+            <h1>Welcome back, <span>{username}</span></h1>
+        </div>
+        {/* part 2: progress bar */}
+        <div className='container-homepage-progress'>
+            <div className='container-homepage-progress-bar'>
+                <div>
+                    <h1>Your overall progress</h1>
+                </div>
+                <div className='container-homepage-progress-bar-line'>
+                    <div className="progress-bar" style={progressStyle}></div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
