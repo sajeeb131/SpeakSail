8e05107901408b472bff62a9e94c06f0374b2a47
sajeeb131 <sajeeb131sarkar@gmail.com>
2024-02-06 23:25:32 +0600

    homepage dynamic part

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -9,6 +9,24 @@ const HomePage = () => {
     const progressStyle = {
         width: `${overallProgress}%`
     };
+    const [listeningProgress, setListeningProgress] = useState(60);
+    const [speakingProgress, setSpeakingProgress] = useState(45);
+    const [readingProgress, setReadingProgress] = useState(28);
+    const [writingProgress, setWritingProgress] = useState(10);
+    const progressStyles = {
+        listening: {
+            width: `${listeningProgress}%`
+        },
+        speaking: {
+            width: `${speakingProgress}%`
+        },
+        reading: {
+            width: `${readingProgress}%`
+        },
+        writing: {
+            width: `${writingProgress}%`
+        }
+    };
   return (
     <div className='container-homepage'>
         <Navbar/>
@@ -27,6 +45,44 @@ const HomePage = () => {
                 </div>
             </div>
         </div>
+        {/* part 3: vocab treasure, daily mission */}
+        <div></div>
+        
+        {/* part 4: lessons */}
+        <div className='container-homepage-lessons'>
+            <div>
+                <h1>Lessons</h1>
+            </div>
+            <div className='container-homepage-lessons-half'>
+                <div className='container-homepage-lessons-indv'>
+                    <h2>Listening</h2>
+                    <div className='container-homepage-progress-bar-line'>
+                        <div className="progress-bar" style={progressStyles.listening}></div>
+                    </div>
+                </div>
+                <div className='container-homepage-lessons-indv'>
+                    <h2>Speaking</h2>
+                    <div className='container-homepage-progress-bar-line'>
+                        <div className="progress-bar" style={progressStyles.speaking}></div>
+                    </div>
+                </div>
+            </div>
+            <div className='container-homepage-lessons-half'>
+                <div className='container-homepage-lessons-indv'>
+                    <h2>Reading</h2>
+                    <div className='container-homepage-progress-bar-line'>
+                        <div className="progress-bar" style={progressStyles.reading}></div>
+                    </div>
+                </div>
+                <div className='container-homepage-lessons-indv'>
+                    <h2>Writing</h2>
+                    <div className='container-homepage-progress-bar-line'>
+                        <div className="progress-bar" style={progressStyles.writing}></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* part 5: materials */}
     </div>
   )
 }
