5ba6db13e3478d9d551786f54a086ec0fe546ae2
sajeeb131 <sajeeb131sarkar@gmail.com>
2024-02-12 23:25:08 +0600

    listening main page added.

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,27 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 //Pages and Components
 import LandingPage from './pages/landing-page/LandingPage';
 import HomePage from './pages/home/HomePage'
-
+import ListeningMain from './pages/listeningMain/ListeningMain';
+import Status503 from './pages/Status/Status503';
 function App() {
   return (
-    <div className="App">
+    <div className="App">       
         <BrowserRouter>
+          {/* Error pages */}
+          <Routes>
+              <Route path='/503' element={<Status503/>}/>
+          </Routes>
+
           <Routes>
             <Route path='/' element={<LandingPage/>}/>
           </Routes>
+
           <Routes>
             <Route path='/home' element={<HomePage/>}/>
+          </Routes>
+
+          <Routes>
+            <Route path='/listening-main' element={<ListeningMain/>}/>
           </Routes>
         </BrowserRouter>
     </div>
---
+++ client/src/pages/Status/Status503.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Status503 = () => {
+  return (
+    <div>
+      <h1>503 Service Unavailable</h1>
+    </div>
+  )
+}
+
+export default Status503
