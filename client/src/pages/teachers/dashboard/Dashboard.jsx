46a5aa2a86d63cbe44db842d099a26d4b0391589
kh-abrar <kh.abrarr@gmail.com>
2024-02-13 23:22:59 +0600

    dashboard p1

--- client/src/pages/teachers/dashboard/Dashboard.jsx
+++ client/src/pages/teachers/dashboard/Dashboard.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import Navbar from '../../../components/teacherNav'
+import './Dashboard.css'
+
+
 const Dashboard = () => {
   return (
-    <Navbar></Navbar>
+    <div className='page-container'>
+      <div className="navbar-container">
+        <Navbar/>
+      </div>
+      <div className="content-beside-navbar">
+        <div className='top'>
+          <input type="text" className="search-field" placeholder="Search..." />
+        </div>
+        <div className='middle'>
+          <div className='middle-left'>
+            
+          </div>
+          <div className='middle-right'>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
