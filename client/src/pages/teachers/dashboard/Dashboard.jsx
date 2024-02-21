fc1de6d438df5d4ca264234c74bc46a8c42f2ad7
kh-abrar <kh.abrarr@gmail.com>
2024-02-21 15:02:44 +0600

    dashboard middle completed

--- client/src/pages/teachers/dashboard/Dashboard.jsx
+++ client/src/pages/teachers/dashboard/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Navbar from '../../../components/teacherNav'
 import './Dashboard.css'
+import { Link, useNavigate } from 'react-router-dom';
+import avatar1 from '../../../assets/images/PFP.png'
 import { DUMMY_POST } from './data'
 import { DUMMY_POST1 } from './data2'
 
@@ -8,8 +10,9 @@ import { DUMMY_POST1 } from './data2'
 const Dashboard = () => {
   const [totalStudents, setTotalStudents] = useState(0);
   const [totalClasses, setTotalClasses] = useState(5);
-  const [topStudents, setTopStudents] = useState(DUMMY_POST)
-  const [recents, setRecents] = useState(DUMMY_POST1)
+  const [studentImg, setStudentImg] = useState(avatar1);
+  const [topStudents, setTopStudents] = useState(DUMMY_POST);
+  const [recents, setRecents] = useState(DUMMY_POST1);
   return (
     <div className='page-container'>
       <div className="navbar-container">
@@ -37,22 +40,26 @@ const Dashboard = () => {
                 <h1>{totalStudents}</h1>
               </div>
             </div>
-
+            <h2 id='headers2'>Top performing students</h2>
             <div className='mid'>
-              <h2>Top performing students</h2>
-              {topStudents.map((Item, key)=>{
+              {topStudents.slice(0,4).map((Item, key)=>{
                 return(
                   <div className='student-info'>
-                      <h2>Name: {Item.name}</h2>
-                      <h3>Class: {Item.class}</h3>
-                      <h3>{Item.completed}%</h3>
+                      <div className='img-block'>
+                        <img src={studentImg}></img>
+                      </div>
+                      <div className='info-block'>
+                        <h2>Name: {Item.name}</h2>
+                        <h3>Class: {Item.class}</h3>
+                        <h3>{Item.completed}%</h3>
+                      </div>
                   </div>
                 )
               })}
+              <Link to='/teachers/students-list'>See more</Link>
             </div>
-
+            <h2 id='headers2'>Recently accessed classes</h2>
             <div className='bot'>
-                <h2>Recently accessed classes</h2>
                 {recents.slice(0,2).map((item, key)=>{
                   return(
                     <div className='class-info'>
