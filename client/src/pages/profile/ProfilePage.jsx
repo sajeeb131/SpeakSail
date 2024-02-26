571cf6fd276955d0f3f702e1b05916990b46058a
Yeasin Hasan <yeasinhasan9822@gmail.com>
2024-02-27 03:01:55 +0600

    profile - achievements, stats added - done

--- client/src/pages/profile/ProfilePage.jsx
+++ client/src/pages/profile/ProfilePage.jsx
@@ -8,11 +8,16 @@ import listeningBadge from "../../assets/images/listening.png"
 import speakingBadge from "../../assets/images/speaking.jpg"
 import writingBadge from "../../assets/images/writing.png"
 import readingBadge from "../../assets/images/reading.jpg"
+import badge1 from "../../assets/images/badge1.png"
+import badge2 from "../../assets/images/badge2.png"
+import badge3 from "../../assets/images/badge3.png"
+import badge4 from "../../assets/images/badge4.png"
+
 
 const ProfilePage = () => {
     const student = {
         name: 'Ralph Lauren',
-        id: '0123143',
+        id: '@0123143',
         school: 'South Park Elementary',
         class: '3',
         section: 'A',
@@ -74,13 +79,24 @@ const ProfilePage = () => {
                     <h1 className='stats-headerText'>Stats</h1>
                 </div>
                 <div className="lessonsCompletion-stats">
-                    <h3 className='stats-text'>Sentence Dictations:</h3>
-                    <h3 className='stats-text'>Question and Answer:</h3>
-                    <h3 className='stats-text'>Poems:</h3>
-                    <h3 className='stats-text'>Storytelling:</h3>
-                    <h3 className='stats-text'>Digital Story boarding:</h3>
-                    <h3 className='stats-text'>Picture Description:</h3>
-                    <h3 className='stats-text'>Chain Story:</h3>
+                    <div className="lessionsCompletion-stats-lessons">
+                        <h3 className='stats-text'>Sentence Dictations:</h3>
+                        <h3 className='stats-text'>Question and Answer:</h3>
+                        <h3 className='stats-text'>Poems:</h3>
+                        <h3 className='stats-text'>Storytelling:</h3>
+                        <h3 className='stats-text'>Digital Story boarding:</h3>
+                        <h3 className='stats-text'>Picture Description:</h3>
+                        <h3 className='stats-text'>Chain Story:</h3>
+                    </div>
+                    <div className="lessonsCompletion-num">
+                        <h3 className='stats-text'>6/32</h3>
+                        <h3 className='stats-text'>2/32</h3>
+                        <h3 className='stats-text'>5/9</h3>
+                        <h3 className='stats-text'>2/5</h3>
+                        <h3 className='stats-text'>2/3</h3>
+                        <h3 className='stats-text'>6/10</h3>
+                        <h3 className='stats-text'>2/6</h3>
+                    </div>
                 </div>
             </div>
             <div className="profilepage-stats-achievements">
@@ -88,6 +104,10 @@ const ProfilePage = () => {
                     <h1 className='stats-headerText'>Achievements</h1>
                 </div>
                 <div className="profilepage-achievements">
+                    <img className='stats-achievements-image' src={badge1} alt="" />
+                    <img className='stats-achievements-image' src={badge2} alt="" />
+                    <img className='stats-achievements-image' src={badge3} alt="" />
+                    <img className='stats-achievements-image' src={badge4} alt="" />
                     
                 </div>
 
