7d9b386316fb834815b02d3b7b0b61ffda9d5a84
suchetahassin <shassin202029@bscse.uiu.ac.bd>
2024-03-18 13:17:15 +0600

    DailyMission extension changed to .js

---
+++ client/src/pages/daily-mission/DailyMission.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import './DailyMission.css'
