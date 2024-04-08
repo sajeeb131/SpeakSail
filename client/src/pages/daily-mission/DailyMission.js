4a54f49df75eddd0ee945b13fcc049d9d7852e1f
suchetahassin <shassin202029@bscse.uiu.ac.bd>
2024-04-08 13:54:38 +0600

    DailyMission.js to .jsx

--- client/src/pages/daily-mission/DailyMission.js
+++
@@ -1,5 +0,0 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
-import './DailyMission.css'
