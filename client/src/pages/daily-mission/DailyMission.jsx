38e58d826de1ced5270d4bad6a1cc9b42c1ab1e0
suchetahassin <shassin202029@bscse.uiu.ac.bd>
2024-05-25 19:20:41 +0600

    DailyMission without footer

--- client/src/pages/daily-mission/DailyMission.jsx
+++ client/src/pages/daily-mission/DailyMission.jsx
@@ -8,29 +8,44 @@ const DailyMission = () => {
     return (
         <div className="daily-mission">
             <Navbar />
-            <section className="daily-mission-inner">
-                <div className="whole-container">
-                    <div className="lesson-heading">
-                        <div className="heading">
-                            <div className="daily-heading">
-                                <h2 className="daily">Daily</h2>
-                            </div>
-
-                            <div className="mission-heading">
-                                <div className="mission">Mission</div>
-                            </div>
+                <div className="lesson-heading">
+                    <div className="heading">
+                        <div className="daily-heading">
+                            <h2 className="daily">Daily</h2>
                         </div>
 
-                        {/* Main Section */}
-                        <div className="main-section">
-                            <div className="task-instruction">
-                                <h4>Complete the following sentence by using the correct word.</h4>
-                            </div>
+                        <div className="mission-heading">
+                            <div className="mission">Mission</div>
                         </div>
+                    </div>
+                </div>    
+
+                {/* Main Section */}
+                <div className="main-section">
+                    <div className="task-instruction">
+                        <h4>Complete the following sentence by using the correct word.</h4>
+                    </div>
+
+                    <div className="answer-options-box">
+                        <span className="answer-option">{'frog'}</span>
+                        <span className="answer-option">{'after'}</span>
+                        <span className="answer-option">{'sing'}</span>
+                        <span className="answer-option">{'before'}</span>
+
+                    </div>
+
+                    <div className="questions">
+                        <p>1. I like to <input type="text" name="answer" id="answer" /> and play the piano.</p>
+                        <p>2. We went to bed <input type="text" name="answer" id="answer" /> watching TV.</p>
+
+                    </div>
+
+                    <div className="submission-buttons">
+                        <button className="skip-button">Skip</button>
+                        <button className="submit-button">Submit</button>
 
                     </div>
                 </div>
-            </section>
 
         </div>
     );
