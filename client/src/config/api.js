49e4a499df9e584bbfa99ebbea2f940021395e45
Sajeeb Sarkar <57650452+sajeeb131@users.noreply.github.com>
2025-01-28 23:06:37 +0600

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000"
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://speaksail-server.onrender.com"
 
